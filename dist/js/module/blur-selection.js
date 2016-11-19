1065ed7c23963bf1ca84110d463ecbb97f66cb1b
Jia <normal999@163.com>
2016-11-19 19:08:55 +0800

    Add files via upload
    下拉框模糊查询

--- dist/js/module/blur-selection.js
+++ dist/js/module/blur-selection.js
@@ -1,6 +1,6 @@
 /**
  * Created by huangjiajia on 2016/11/18.
- * Descrition: 模糊查询
+ * Description: 模糊查询
  * Function: 1. 单选模糊查询 2. 多选模糊查询
  */
 (function($) {
@@ -16,7 +16,13 @@
         multiSelect: false,
         panelClass : '.js-blur-panel',
         inputClass : '.js-blur-input',
-        wrapClass  : '.js-blur-wrap'
+        wrapClass  : '.js-blur-wrap',
+        ajax       : {
+            type    : 'POST',
+            url     : '',
+            dataType: 'json',
+            data    : ''
+        }
     };
 
     blurSelect.prototype.select = function () {
@@ -25,15 +31,39 @@
          * 1. 判断是否为多选模糊查询  option -> multiSelect 默认为单模糊查询
          */
         if(this.options.multiSelect) { // 多选模糊查询
-
+            this.multiSelect();
         }
         else {  // 单选模糊查询
-
+            this.singleSelect();
         }
     };
 
     blurSelect.prototype.multiSelect = function () {
+        var _this  = this,
+            $panel = _this.$panel;
 
+        /*
+         * 1. 创建查询结果面板
+         * 2. 监听input keyup事件
+         * 3. 遍历结果
+         */
+        _this.createResultHtml();
+        $panel.on('keyup', _this.inputClass, function () {
+            var $this    = $(this),
+                value    = $this.val(),
+                ajaxType = _this.options.ajax;
+
+            $.ajax({
+                type: ajaxType.type,
+                dataType: ajaxType.dataType,
+                url: ajaxType.url,
+                data: {value}
+            })
+        });
+    };
+
+    blurSelect.prototype.singleSelect = function () {
+        
     };
 
     blurSelect.prototype.createResultHtml = function () {
